c02cdfda3ab3eb99783a4124104ade79d29d5f52
suroor <surusuroor@gmail.com>
2017-03-12 11:22:27 +0530

    Changes name of config tokens for feature path.
    This is done so that we can use the config to define all our settings
    and eliminate need for command line arguments.
    This is also required for integrating the project into a web framework.

--- object-detector/extract-features.py
+++ object-detector/extract-features.py
@@ -26,12 +26,12 @@ if __name__ == "__main__":
     des_type = args["descriptor"]
 
     # If feature directories don't exist, create them
-    if not os.path.isdir(pos_feat_ph):
-        os.makedirs(pos_feat_ph)
+    if not os.path.isdir(pos_feat_path):
+        os.makedirs(pos_feat_path)
 
     # If feature directories don't exist, create them
-    if not os.path.isdir(neg_feat_ph):
-        os.makedirs(neg_feat_ph)
+    if not os.path.isdir(neg_feat_path):
+        os.makedirs(neg_feat_path)
 
     print "Calculating the descriptors for the positive samples and saving them"
     for im_path in glob.glob(os.path.join(pos_im_path, "*")):
@@ -39,9 +39,9 @@ if __name__ == "__main__":
         if des_type == "HOG":
             fd = hog(im, orientations, pixels_per_cell, cells_per_block, visualize, normalize)
         fd_name = os.path.split(im_path)[1].split(".")[0] + ".feat"
-        fd_path = os.path.join(pos_feat_ph, fd_name)
+        fd_path = os.path.join(pos_feat_path, fd_name)
         joblib.dump(fd, fd_path)
-    print "Positive features saved in {}".format(pos_feat_ph)
+    print "Positive features saved in {}".format(pos_feat_path)
 
     print "Calculating the descriptors for the negative samples and saving them"
     for im_path in glob.glob(os.path.join(neg_im_path, "*")):
@@ -49,8 +49,8 @@ if __name__ == "__main__":
         if des_type == "HOG":
             fd = hog(im,  orientations, pixels_per_cell, cells_per_block, visualize, normalize)
         fd_name = os.path.split(im_path)[1].split(".")[0] + ".feat"
-        fd_path = os.path.join(neg_feat_ph, fd_name)
+        fd_path = os.path.join(neg_feat_path, fd_name)
         joblib.dump(fd, fd_path)
-    print "Negative features saved in {}".format(neg_feat_ph)
+    print "Negative features saved in {}".format(neg_feat_path)
 
     print "Completed calculating features from training images"
