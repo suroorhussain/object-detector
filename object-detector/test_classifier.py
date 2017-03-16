0f877c28bd3f55eab90f6f7adde917df9b91a26f
suroor <surusuroor@gmail.com>
2017-03-16 11:34:02 +0530

    Changes the structure of the script so that detect() can imported and also keep the script as statndalone.

--- object-detector/test_classifier.py
+++ object-detector/test_classifier.py
@@ -29,13 +29,12 @@ def sliding_window(image, window_size, step_size):
         for x in xrange(0, image.shape[1], step_size[0]):
             yield (x, y, image[y:y + window_size[1], x:x + window_size[0]])
 
-def detect(image):
+def detect(image, visualize_det=False):
     # Read the image
     im = imread(image, as_grey=False)
     min_wdw_sz = (100, 40)
     step_size = (10, 10)
     downscale = 1.25
-    visualize_det = True
 
     # Load the classifier
     clf = joblib.load(model_path)
@@ -86,8 +85,9 @@ def detect(image):
     for (x_tl, y_tl, _, w, h) in detections:
         # Draw the detections
         cv2.rectangle(im, (x_tl, y_tl), (x_tl+w, y_tl+h), (0, 0, 0), thickness=2)
-    cv2.imshow("Raw Detections before NMS", im)
-    cv2.waitKey()
+    if visualize_det:
+        cv2.imshow("Raw Detections before NMS", im)
+        cv2.waitKey()
 
     # Perform Non Maxima Suppression
     detections = nms(detections, threshold)
@@ -96,8 +96,9 @@ def detect(image):
     for (x_tl, y_tl, _, w, h) in detections:
         # Draw the detections
         cv2.rectangle(clone, (x_tl, y_tl), (x_tl+w,y_tl+h), (0, 0, 0), thickness=2)
-    cv2.imshow("Final Detections after applying NMS", clone)
-    cv2.waitKey()
+    if visualize_det:
+        cv2.imshow("Final Detections after applying NMS", clone)
+        cv2.waitKey()
 
 if __name__ == "__main__":
     # Parse the command line arguments
@@ -105,69 +106,4 @@ if __name__ == "__main__":
     parser.add_argument('-i', "--image", help="Path to the test image", required=True)
     args = vars(parser.parse_args())
 
-    im = imread(args["image"], as_grey=False)
-    min_wdw_sz = (100, 40)
-    step_size = (10, 10)
-    downscale = 1.25
-    visualize_det = True
-
-    # Load the classifier
-    clf = joblib.load(model_path)
-
-    # List to store the detections
-    detections = []
-    # The current scale of the image
-    scale = 0
-    # Downscale the image and iterate
-    for im_scaled in pyramid_gaussian(im, downscale=downscale):
-        # This list contains detections at the current scale
-        cd = []
-        # If the width or height of the scaled image is less than
-        # the width or height of the window, then end the iterations.
-        if im_scaled.shape[0] < min_wdw_sz[1] or im_scaled.shape[1] < min_wdw_sz[0]:
-            break
-        for (x, y, im_window) in sliding_window(im_scaled, min_wdw_sz, step_size):
-            if im_window.shape[0] != min_wdw_sz[1] or im_window.shape[1] != min_wdw_sz[0]:
-                continue
-            # Calculate the HOG features
-            fd = hog(im_window, orientations, pixels_per_cell, cells_per_block, visualize, normalize)
-            pred = clf.predict(fd)
-            if pred == 1:
-                print  "Detection:: Location -> ({}, {})".format(x, y)
-                print "Scale ->  {} | Confidence Score {} \n".format(scale,clf.decision_function(fd))
-                detections.append((x, y, clf.decision_function(fd),
-                    int(min_wdw_sz[0]*(downscale**scale)),
-                    int(min_wdw_sz[1]*(downscale**scale))))
-                cd.append(detections[-1])
-            # If visualize is set to true, display the working
-            # of the sliding window
-            if visualize_det:
-                clone = im_scaled.copy()
-                for x1, y1, _, _, _  in cd:
-                    # Draw the detections at this scale
-                    cv2.rectangle(clone, (x1, y1), (x1 + im_window.shape[1], y1 +
-                        im_window.shape[0]), (0, 0, 0), thickness=2)
-                cv2.rectangle(clone, (x, y), (x + im_window.shape[1], y +
-                    im_window.shape[0]), (255, 255, 255), thickness=2)
-                cv2.imshow("Sliding Window in Progress", clone)
-                cv2.waitKey(30)
-        # Move the the next scale
-        scale+=1
-
-    # Display the results before performing NMS
-    clone = im.copy()
-    for (x_tl, y_tl, _, w, h) in detections:
-        # Draw the detections
-        cv2.rectangle(im, (x_tl, y_tl), (x_tl+w, y_tl+h), (0, 0, 0), thickness=2)
-    cv2.imshow("Raw Detections before NMS", im)
-    cv2.waitKey()
-
-    # Perform Non Maxima Suppression
-    detections = nms(detections, threshold)
-
-    # Display the results after performing NMS
-    for (x_tl, y_tl, _, w, h) in detections:
-        # Draw the detections
-        cv2.rectangle(clone, (x_tl, y_tl), (x_tl+w,y_tl+h), (0, 0, 0), thickness=2)
-    cv2.imshow("Final Detections after applying NMS", clone)
-    cv2.waitKey()
+    detect(args["image"], visualize_det=True)
